f7246fe385747d4519db320378425130f41f1247
Matheus Ferreira <matheus.fer33@gmail.com>
2022-04-06 19:11:12 -0300

    layout pronto, com inputs checkbox inseridos. falta agora a lógica

--- src/components/Cartela/Cartela.jsx
+++ src/components/Cartela/Cartela.jsx
@@ -7,17 +7,19 @@ for (let i = 1; i <= 60; i++) {
    cartela.push(('0' + i).slice(-2))
 }
 
-console.log(cartela)
+// console.log(cartela)
 
 export default function Cartela() {
-
 
   return (
     <form className="num-wrapper">
       <h2>Megasena</h2>
       <div className='numbers'>
         {cartela.map((number) => (
-          <h6 key={number}>{number}</h6>
+          <label key={number}>
+            <input type='checkbox'></input>
+            <span>{number}</span>
+          </label>
         ))}
       </div>
     </form>
