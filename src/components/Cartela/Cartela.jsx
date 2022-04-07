24255391c9fcf14fb72f3cad056d6ea00266999d
Matheus Ferreira <matheus.fer33@gmail.com>
2022-04-06 23:11:24 -0300

    logica para seleção dos numeros e criação dos numeros sorteados

--- src/components/Cartela/Cartela.jsx
+++ src/components/Cartela/Cartela.jsx
@@ -1,26 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Cartela.scss'
-
-let cartela = []
-
-for (let i = 1; i <= 60; i++) {
-   cartela.push(('0' + i).slice(-2))
-}
-
-// console.log(cartela)
 
 export default function Cartela() {
 
+  let cartela = []
+
+  for (let i = 1; i <= 60; i++) {
+    cartela.push(('0' + i).slice(-2))
+  }
+
+  const [numbers, setNumbers] = useState([])
+  const [flag, setFlag] = useState(false)
+
+  const handleChange = (event) => {
+    const { value, checked } = event.target;
+    
+    if (checked) {
+      setNumbers([...numbers, value])
+
+    } else {
+      setNumbers(numbers.filter((e) => e !== value))
+    }
+  }
+
+  function showResult() {
+    const result = numbers
+    if (result.length > 6 ) {
+      alert('Escolha no máximo 6 números');
+    } else {
+      generateRandomSix();
+      setFlag(true)
+    }
+  }
+
+  function generateRandomSix() {
+    
+    const myArray = [];
+    while (myArray.length <= 5) {
+      const aleatorio = parseInt(Math.random() * (61 - 1) + 1)
+
+      if (myArray.includes(aleatorio)) {
+        generateRandomSix()
+      } else {
+        myArray.push(aleatorio)
+      }
+    }
+    console.log(myArray.sort())
+
+    return (myArray.sort((n1,n2) => n1 - n2)).toString().replaceAll(',', ' ')
+  }
+ 
   return (
     <form className="num-wrapper">
       <h2>Megasena</h2>
       <div className='numbers'>
         {cartela.map((number) => (
           <label key={number}>
-            <input type='checkbox'></input>
+            <input
+              type='checkbox'
+              value={number}
+              onChange={handleChange}
+            />
             <span>{number}</span>
           </label>
         ))}
+      </div>
+      <label >
+        <input value={numbers} type="text" disabled/>
+      </label>
+      <button onClick={showResult} type='button'>Gerar números aleatórios</button>
+      <div className="result">
+          {flag ? generateRandomSix() : ''}
       </div>
     </form>
   )
